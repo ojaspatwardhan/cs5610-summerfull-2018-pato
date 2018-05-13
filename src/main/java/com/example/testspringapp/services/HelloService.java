5d176e56153c3852189678c19d59761fc09b36d2
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-05-14 02:43:32 +0530

    Inserted deleteHello in HelloService.java

--- src/main/java/com/example/testspringapp/services/HelloService.java
+++ src/main/java/com/example/testspringapp/services/HelloService.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.testspringapp.models.Hello;
 import com.example.testspringapp.repositories.HelloRepository;
@@ -23,6 +25,11 @@ public class HelloService {
 	public Hello createHello(@RequestBody Hello hello) {
 		return repository.save(hello);
 		
+	}
+	
+	@DeleteMapping("/api/hello/{helloId}")
+	public void deleteHello(@PathVariable("helloId") int id) {
+		repository.deleteById(id);
 	}
 
 
