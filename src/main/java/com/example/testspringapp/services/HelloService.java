8c0c167bbce4d2566b4ff2d4f529ce3530524022
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-05-14 02:29:52 +0530

    Added createHello() method

--- src/main/java/com/example/testspringapp/services/HelloService.java
+++ src/main/java/com/example/testspringapp/services/HelloService.java
@@ -3,6 +3,8 @@ package com.example.testspringapp.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.testspringapp.models.Hello;
 import com.example.testspringapp.repositories.HelloRepository;
@@ -15,6 +17,12 @@ public class HelloService {
 	@GetMapping("/api/hello")
 	public Iterable<Hello> findAllHellos() {
 		return repository.findAll();
+	}
+	
+	@PostMapping("/api/hello")
+	public Hello createHello(@RequestBody Hello hello) {
+		return repository.save(hello);
+		
 	}
 
 
