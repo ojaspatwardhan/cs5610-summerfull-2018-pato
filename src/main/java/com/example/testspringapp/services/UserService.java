030f6d14975edc8efa5fa9f8d81026ffe85da613
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-05-14 18:11:44 +0530

    Added a post mapping and a get mapping to UserService to fetch and render data from database and to add new users to the database. Also added jQuery methods in user-admin.controller.client.js to do the above functions from the DOM.

--- src/main/java/com/example/testspringapp/services/UserService.java
+++ src/main/java/com/example/testspringapp/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.testspringapp.models.User;
 import com.example.testspringapp.repositories.UserRepository;
@@ -13,6 +15,11 @@ public class UserService {
 	
 	@Autowired
 	UserRepository repository;
+	
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
+	}
 	
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
--- src/main/webapp/jQuery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jQuery/components/user-admin/user-admin.controller.client.js
@@ -2,12 +2,52 @@
   'use strict';
   jQuery(main);
 
-  var users = fetch("http://localhost:8080/api/user");
+  var tbody;
+  var repeat;
 
   function main() {
+    var promise = fetch("http://localhost:8080/api/user");
+
+    tbody = $("tbody");
+    repeat = $(".user-credentials")
+    promise.then(function (response) {
+      return response.json();
+    }).then(renderUsers)
+
+    $("#createNewUser").click(createUser)
+  }
+
+  function createUser() {
+    var firstName = $("#firstNameFld").val();
+    var lastName = $("#lastNameFld").val();
+    var username = $("#usernameFld").val();
+    var password = $("#passwordFld").val();
+
+    var user = {
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      password: password
+    };
+
+    fetch("http://localhost:8080/api/user", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "content-type": "application/json"
+      }
+    });
   }
 
   function renderUsers(users) {
-
+    for(var i = 0; i < users.length; i++) {
+      var user = users[i];
+      var newEntry = repeat.clone();
+      newEntry.find(".userName").html(user.username);
+      newEntry.find(".firstName").html(user.firstName);
+      newEntry.find(".lastName").html(user.lastName);
+      newEntry.find(".password").html(user.password);
+      tbody.append(newEntry);
+    }
   }
 }());
