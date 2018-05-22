2422d525e191217e6b0bc533bef9a17508e2e88f
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-05-22 19:33:58 +0530

    Centered Login and Register pages and fixed a search user bug in user-admin.template.client.html

--- src/main/webapp/jQuery/components/forgotPassword/forgotPassword.controller.client.js
+++ src/main/webapp/jQuery/components/forgotPassword/forgotPassword.controller.client.js
@@ -6,9 +6,10 @@
   function init() {
     $("#resetPasswordBtn").click(function() {
       var email = $("#forgotPasswordEmail").val();
-      var user = {
-        email: email
-      };
+      var user = new User("", "", "", "", "", "", "", email);
+      // var user = {
+      //   email: email
+      // };
       resetPassword(user);
     });
   }
@@ -18,12 +19,17 @@
     userService.findUserByEmail(user.email, redirect);
   }
 
-  function redirect() {
-    // localhost location
-    window.location = "http://localhost:8080/jQuery/components/login/login.template.client.html";
+  function redirect(response) {
+    if(response.length != 0) {
+      // localhost location
+      window.location = "http://localhost:8080/jQuery/components/login/login.template.client.html";
 
-    // Heroku Location
-    // window.location = "https://cs5610-summer-2018-pat-ojas.herokuapp.com/jQuery/components/login/login.template.client.html"
+      // Heroku Location
+      // window.location = "https://cs5610-summer-2018-pat-ojas.herokuapp.com/jQuery/components/login/login.template.client.html"
+    }
+    else {
+      alert("Enter a valid email");
+    }
   }
 
 }());
